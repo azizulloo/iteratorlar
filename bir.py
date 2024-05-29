36489431feb325046e15232e57157e6a33511322
[200~welcome~ <temirovazizullo2004@gmail.com>
2024-05-29 20:48:04 +0500

    iteratorlar

---
+++ bir.py
@@ -0,0 +1,24 @@
+class Raqamlar:
+    def __iter__(self):
+        self.a=1
+        return self
+    
+    def __next__(self):
+        x=self.a
+        self.a+=1
+        return x
+    
+myclass=Raqamlar()
+myiter=iter(myclass)
+
+
+
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+
+    
+
